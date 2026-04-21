7fcd0ba2e6b55375c0244488937163565f839bad
AlexZhonn <zhuen.zhong@outlook.com>
2026-04-20 20:59:51 -0400

    fix to allow all Vercel preview URLs

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,13 @@ import recipeRoutes from "./routes/recipeRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const clientUrl = process.env.CLIENT_URL;
-if (!clientUrl) {
-  throw new Error("Missing CLIENT_URL in backend/.env");
-}
 
-app.use(cors({ origin: process.env.CLIENT_URL }));
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:5173",
+  /\.vercel\.app$/,
+];
+app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
